26e5575bfe3f224751c7664883c69df799b6542e
Eldengawy <eldengaawy@gmail.com>
2025-09-11 12:07:15 +0300

    refactor(buttons): simplify custom back button implementation by using custom icon button
    Replace the complex InkWell and Container implementation with CustomIconButton for better maintainability and consistency with other button components in the codebase.
    
    [build] [apk]

--- lib/core/utils/widgets/buttons/custom_back_button.dart
+++ lib/core/utils/widgets/buttons/custom_back_button.dart
@@ -1,57 +1,43 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:must_invest_service_man/core/static/icons.dart';
 import 'package:must_invest_service_man/core/theme/colors.dart';
-import 'package:must_invest_service_man/core/utils/widgets/long_press_effect.dart';
+import 'package:must_invest_service_man/core/utils/widgets/buttons/custom_icon_button.dart';
 
 class CustomBackButton extends StatelessWidget {
   final Color? color;
+  final Color? iconColor;
   final VoidCallback? onTap;
-  const CustomBackButton({super.key, this.onTap, this.color = const Color(0xffEAEAF3)});
+
+  const CustomBackButton({super.key, this.onTap, this.color = const Color(0xffEAEAF3), this.iconColor});
 
   @override
   Widget build(BuildContext context) {
     return Hero(
       tag: 'back',
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          borderRadius: BorderRadius.circular(10),
-          onTap: () {
+      child: CustomIconButton(
+        iconAsset: AppIcons.arrowIc, 
+        iconColor:
+            iconColor ??
+            (color == AppColors.primary.withValues(alpha: 0.4)
+                ? Colors.black
+                : AppColors.primary.withValues(alpha: 0.4)),
+        color: color ?? Colors.transparent,
+        boxShadow: const BoxShadow(color: Color(0x07000000), blurRadius: 4, offset: Offset(0, 3), spreadRadius: 0),
+        onPressed: () {
+          if (onTap != null) {
+            onTap!();
+          } else {
             if (Navigator.canPop(context)) {
               Navigator.pop(context);
               log('CustomBackButton: Navigator.pop executed');
             } else {
               log('CustomBackButton: Cannot pop, maybe root screen?');
             }
-          },
-          child: Container(
-            padding: const EdgeInsets.all(9),
-            width: 46,
-            height: 46,
-            decoration: ShapeDecoration(
-              color: color ?? Colors.transparent,
-              shape: RoundedRectangleBorder(
-                // side: const BorderSide(width: 1, color: Color(0xFFF1F1F2)),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              shadows: const [
-                BoxShadow(color: Color(0x07000000), blurRadius: 4, offset: Offset(0, 3), spreadRadius: 0),
-              ],
-            ),
-            child: Center(
-              child: Icon(
-                Icons.arrow_back_ios_new_rounded,
-                color:
-                    color == AppColors.primary.withValues(alpha: 0.4)
-                        ? Colors.black
-                        : AppColors.primary.withValues(alpha: 0.4),
-                size: 20,
-              ),
-            ),
-          ),
-        ),
+          }
+        },
       ),
-    ).withPressEffect();
+    );
   }
 }
